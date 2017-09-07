ce5643d646ee3182981159a23083286e4311c7a1
xioamafan <993144563+xiaomafan@users.noreply.github.com>
2017-09-07 19:31:30 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+   
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
